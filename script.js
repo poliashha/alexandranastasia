4fd48b69eacc0cd30755c95f95f86d07407ae56e
Polina Spirina <polly_sp@mail.ru>
2026-05-19 18:26:11 +0300

    good

---
+++ script.js
@@ -0,0 +1,170 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbz8dmiVNZqqIlGyaBmXUFN6gW1GnNuW_fVj0EBubSK1wnFxZv34tzK9DLTdYG2hfsDG_w/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const formSendResult = document.querySelector(".form-send");
+  formSendResult.textContent = "";
+  const drinks = formData.getAll("drinks");
+  const submitButton = document.querySelector(".button");
+  submitButton.textContent = "отправка...";
+  // Преобразуем массив в строку с разделителем (например, запятая)
+  const drinksString = drinks.join(", ");
+
+  // Создаем новый FormData и добавляем все поля
+  const newFormData = new FormData();
+  newFormData.append("name", formData.get("name"));
+  newFormData.append("presence", formData.get("presence"));
+  newFormData.append("eat", formData.get("eat") || "-");
+  newFormData.append("allergy", formData.get("allergy") || "-");
+  newFormData.append("listallergy", formData.get("listallergy") || "-");
+  newFormData.append("cauple", formData.get("cauple") || "-");
+  newFormData.append("listcauple", formData.get("listcauple") || "-");
+  newFormData.append("drinks", drinksString);
+  newFormData.append("child", formData.get("child") || "-");
+  try {
+    const response = await fetch(scriptURL, {
+      method: "POST",
+      body: newFormData,
+    });
+    formSendResult.textContent = "Спасибо! Анкета отправлена.";
+    form.reset(); // Очищаем форму
+  } catch (error) {
+    formSendResult.textContent = "Повторите попытку позже.";
+    console.error(error);
+  } finally {
+    // Возвращаем кнопку в исходное состояние
+    submitButton.textContent = "отправить";
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const yesRadio = document.getElementById("allergyYes");
+  const noRadio = document.getElementById("allergyNo");
+  const allergyBlock = document.getElementById("allergyDetails");
+
+  function toggleAllergyField() {
+    if (yesRadio.checked) {
+      allergyBlock.classList.add("show");
+    } else {
+      allergyBlock.classList.remove("show");
+    }
+  }
+
+  yesRadio.addEventListener("change", toggleAllergyField);
+  noRadio.addEventListener("change", toggleAllergyField);
+  toggleAllergyField();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const yescauple = document.getElementById("caupleYes");
+  const nocauple = document.getElementById("caupleNo");
+  const caupleBlock = document.getElementById("caupleDetails");
+
+  function togglecaupleField() {
+    if (yescauple.checked) {
+      caupleBlock.classList.add("show");
+    } else {
+      caupleBlock.classList.remove("show");
+    }
+  }
+
+  yescauple.addEventListener("change", togglecaupleField);
+  nocauple.addEventListener("change", togglecaupleField);
+  togglecaupleField();
+});
+
+const nameInput = document.getElementById("name");
+const errorElement = document.getElementById("error-text");
+
+nameInput.addEventListener("invalid", function (event) {
+  event.preventDefault();
+  if (this.validity.valueMissing) {
+    errorElement.classList.add("show");
+  }
+});
+
+nameInput.addEventListener("input", function () {
+  if (this.value.trim() !== "") {
+    errorElement.classList.remove("show");
+  }
+});
+
+document.querySelectorAll('input[name="presence"]').forEach((radio) => {
+  radio.addEventListener("invalid", function (e) {
+    e.preventDefault();
+    document.getElementById("presenceError").classList.add("show");
+    return false;
+  });
+});
+
+document.querySelectorAll('input[name="presence"]').forEach((radio) => {
+  radio.addEventListener("change", function () {
+    document.getElementById("presenceError").classList.remove("show");
+  });
+});
+document.querySelectorAll('input[name="cauple"]').forEach((radio) => {
+  radio.addEventListener("invalid", function (e) {
+    e.preventDefault();
+    document.getElementById("presenceError").classList.add("show");
+    return false;
+  });
+});
+
+document.querySelectorAll('input[name="cauple"]').forEach((radio) => {
+  radio.addEventListener("change", function () {
+    document.getElementById("presenceError").classList.remove("show");
+  });
+});
+
+function startCountdown(targetDate) {
+  function updateTimer() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    if (distance < 0) {
+      document.getElementById("timer").style.display = "none";
+      document.getElementById("datetime").textContent = "Мы стали семьей!";
+      return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+  }
+
+  updateTimer();
+  setInterval(updateTimer, 1000);
+}
+
+const newYear = new Date(2026, 6, 23, 16, 0, 0).getTime();
+startCountdown(newYear);
+
+// Находим ВСЕ кнопки
+const buttons = document.querySelectorAll('.promting-button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    // Находим родительский блок .promting для этой кнопки
+    const promtingContainer = this.closest('.promting');
+    
+    // Внутри этого родителя находим скрытый блок
+    const hiddenBlock = promtingContainer.querySelector('.visually-hidden-promting');
+    
+    // Переключаем класс open у скрытого блока
+    hiddenBlock.classList.toggle('open');
+    
+    // Переключаем класс active у кнопки
+    this.classList.toggle('active');
+  });
+});
